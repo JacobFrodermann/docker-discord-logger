116e4edd3b2737b2aa5d5906ace79fdf522c3c1a
Red-S2 <j.frodermann@gmx.net>
2025-06-30 16:56:59 +0200

    add error highlighting

--- src/main.js
+++ src/main.js
@@ -71,7 +71,12 @@ function monitorContainer(containerName, notifyDiscord) {
             // For simplicity here, we just strip the header and process.
             // A full solution would parse the header to see if it's stdout/stderr.
             const message = chunk.toString('utf8').substring(8).trim();
-            discordWebhook.send(`**[${containerName}]** ${message}`);
+            if (message.includes("error msg")) {
+                discordWebhook.send('**[${containerName}]** \n```fix\n${message}\n```');
+                
+            } else {
+                discordWebhook.send(`**[${containerName}]** ${message}`);
+            }
         });
 
         stream.on('end', () => {
